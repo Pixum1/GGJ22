72c1c6b095fd95a0bd833495d60cfe9197647ffe
Gumbok <lwerthmann@gmx.net>
2022-01-22 18:15:23 +0100

    #3

---
+++ Assets/Scripts/ObjectCollision.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCollision : MonoBehaviour
+{
+    string activeObjTag;
+    float hitPosZ;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        hitPosZ = transform.position.z;
+        activeObjTag = other.tag;
+        Destroy(other.gameObject);
+    }
+    private void Update()
+    {
+        if (activeObjTag != null)
+            Invoke(activeObjTag, 0);
+    }
+    void Obstacle()
+    {
+        float dist = 4;
+        if (transform.position.z > hitPosZ - dist)
+        {
+            transform.position += new Vector3(0, 0, -20f*Time.deltaTime);
+        }
+        else
+        {
+            activeObjTag = null;
+        }
+    }
+    void Obstacle2()
+    {
+        float dist = 4;
+        if (transform.position.z < hitPosZ + dist)
+        {
+            transform.position += new Vector3(0, 0, 20f * Time.deltaTime);
+        }
+        else
+        {
+            activeObjTag = null;
+        }
+    }
+}
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField]
-    GameObject obstacle;
+    List<GameObject> obstacles;
     Vector3 point;
     float laneWidth;
     [SerializeField]
@@ -27,9 +27,10 @@ public class Spawner : MonoBehaviour
         yield return new WaitForSeconds(spawnTime);
         if (isActive)
         {
+            int i = Random.Range(0,obstacles.Count);
             float height=1;
-            point = new Vector3(Random.Range(transform.position.x - laneWidth*0.5f+obstacle.transform.lossyScale.x, transform.position.x+ laneWidth * 0.5f - obstacle.transform.lossyScale.x), transform.position.y, transform.position.z);
-            GameObject clone = Instantiate(obstacle, point+new Vector3(0,height*0.5f,0), Quaternion.identity);
+            point = new Vector3(Random.Range(transform.position.x - laneWidth*0.5f+obstacles[i].transform.lossyScale.x, transform.position.x+ laneWidth * 0.5f - obstacles[i].transform.lossyScale.x), transform.position.y, transform.position.z);
+            GameObject clone = Instantiate(obstacles[i], point+new Vector3(0,height*0.5f,0), Quaternion.identity);
             Rigidbody rb=clone.AddComponent<Rigidbody>();
             clone.GetComponent<Collider>().isTrigger = true;
             rb.useGravity = false;
