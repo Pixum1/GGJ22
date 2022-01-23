011b5f4762990cdcf3fa2d15b79a8dc7d7e43793
TobiasK <knaufer@outlook.de>
2022-01-23 15:40:41 +0100

    ui

--- Assets/Scripts/Points.cs
+++ Assets/Scripts/Points.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class Points : MonoBehaviour
 {
     [SerializeField]
-    float p1Points;
+    public float p1Points;
     [SerializeField]
-    float p2Points;
+    public float p2Points;
 
     [SerializeField]
     Transform p1;
@@ -19,8 +19,10 @@ public class Points : MonoBehaviour
 
     private void Update()
     {
-        p1Points += Time.deltaTime * (p1.position.z - wallOfDeath.position.z);
-        p2Points += Time.deltaTime * (p2.position.z - wallOfDeath.position.z);
+        if(p1 != null && p2 != null) {
+            p1Points += Time.deltaTime * (p1.position.z - wallOfDeath.position.z) * 5f;
+            p2Points += Time.deltaTime * (p2.position.z - wallOfDeath.position.z) * 5f;
+        }
     }
 
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -14,6 +14,37 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private Animator gameOverPanelAnimator;
 
+    [SerializeField]
+    private Points points;
+    [SerializeField]
+    private TMP_Text p1_points, p2_points;
+
+    private void Update() {
+        ShowPointsP1();
+        ShowPointsP2();
+    }
+
+    private void ShowPointsP1() {
+        int maxLength = 6;
+
+        p1_points.text = Mathf.RoundToInt(points.p1Points).ToString();
+
+        for (int i = Mathf.RoundToInt(points.p1Points).ToString().Length; i < maxLength; i++) {
+
+            p1_points.text = p1_points.text.Insert(0, "0");
+        }
+    }
+    private void ShowPointsP2() {
+        int maxLength = 6;
+
+        p2_points.text = Mathf.RoundToInt(points.p2Points).ToString();
+
+        for (int i = Mathf.RoundToInt(points.p2Points).ToString().Length; i < maxLength; i++) {
+
+            p2_points.text = p2_points.text.Insert(0, "0");
+        }
+    }
+
     public void ShowGameOverPanel(string _winningPlayer) {
         gameOverPanel.SetActive(true);
         gameOverPanelAnimator.SetTrigger("show");
